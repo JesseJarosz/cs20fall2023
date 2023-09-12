89a808453bfdb60422ffae6e39c33d03e8a6da7e
16054005 <16054005@L815WD220004.edu.cbe.ab.ca>
2023-09-12 09:59:08 -0600

    9:59AM

---
+++ test 1/src/test.java
@@ -0,0 +1,24 @@
+
+public class test {
+
+	public static void main(String[] args) {
+		
+		int age;
+		double cost;
+		String name;
+		char letter;
+		
+		age = 16;
+		cost = 45.50;
+		name = "Jesse";
+		letter = 's';
+		
+		System.out.print(name+"hello"+letter+(age+cost));
+		
+		
+		
+		
+		
+	}
+
+}
